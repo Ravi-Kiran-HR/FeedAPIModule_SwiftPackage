60c29ebd0d60a7f311b3a602e90189a0546b2d14
Ravi Kiran HR <Ravi Kiran>
2022-04-01 21:48:39 +0530

    Commented blank test case

--- Tests/FeedAPIModuleTests/FeedAPIModuleTests.swift
+++ Tests/FeedAPIModuleTests/FeedAPIModuleTests.swift
@@ -2,9 +2,7 @@ import XCTest
 @testable import FeedAPIModule
 
 final class FeedAPIModuleTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-    }
+//    func testExample() throws {
+//
+//    }
 }
